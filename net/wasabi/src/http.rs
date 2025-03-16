e5181eb2461c662cd8dc261b6a361277bf08418e
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2025-03-16 17:49:43 +0100

    init net

---
+++ net/wasabi/src/http.rs
@@ -0,0 +1,15 @@
+extern create alloc;
+use alloc::string::String;
+use saba_core::error::Error;
+use saba_core::http::HttpResponse;
+
+pub struct HttpClient {}
+
+impl HttpClient {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn get(&self, host:String, port:u16, path:String) -> Result<HttpResponse, Error> {}
+}
+
